e239d08091c5d2456b727bc8e9af785116fa90fa
Kevin Hardy <KevinHardy46@gmail.com>
2019-03-01 14:35:51 -0600

    Updates

---
+++ precache-manifest.6632725c6711cf7def1eee127d30f02b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "14e46cb4c53468dced63",
+    "url": "/react-todo-list/static/js/runtime~main.14e46cb4.js"
+  },
+  {
+    "revision": "934696a24faf998f2f2a",
+    "url": "/react-todo-list/static/js/main.934696a2.chunk.js"
+  },
+  {
+    "revision": "c213e3823fa70f7c50ac",
+    "url": "/react-todo-list/static/js/2.c213e382.chunk.js"
+  },
+  {
+    "revision": "934696a24faf998f2f2a",
+    "url": "/react-todo-list/static/css/main.aa453c26.chunk.css"
+  },
+  {
+    "revision": "d87d5ed9457bc578bb9ba00da18bd85f",
+    "url": "/react-todo-list/index.html"
+  }
+];
